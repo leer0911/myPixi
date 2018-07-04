e152f98937f1f53b8797adf1a39fc26b532bad46
lixinxiang <lixinxiang@xiaoyouzi.com>
2018-07-04 14:40:25 +0800

    添加别名

--- src/game.js
+++ src/game.js
@@ -2,16 +2,23 @@ import * as PIXI from 'pixi.js';
 
 export default class Game {
   constructor() {
+    // 添加别名
     this.Application = PIXI.Application;
     this.Sprite = PIXI.Sprite;
     this.app = new this.Application(940, 530);
     this.stage = this.app.stage;
+    this.renderer = this.app.renderer;
+    this.view = this.renderer.view;
     this.loader = PIXI.loader;
     this.resources = PIXI.loader.resources;
+
+    this.ready();
+  }
+  ready() {
     this.loader.add(['assets/img/ui/menu-background.jpg']).load(() => {
       this.initGame();
     });
-    document.querySelector('body').appendChild(this.app.renderer.view);
+    document.querySelector('body').appendChild(this.view);
   }
   initGame() {
     this.loadBackgrounds();
